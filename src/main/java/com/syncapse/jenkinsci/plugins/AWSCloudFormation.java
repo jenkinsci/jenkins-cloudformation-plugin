295118d3fc995a0643f61fff5ae9f169f69ac6d1
Erick Dovale <e.dovale@syncapse.com>
2011-06-30 13:05:58 -0400

    Adding parameters to builder.

---
+++ src/main/java/com/syncapse/jenkinsci/plugins/AWSCloudFormation.java
@@ -0,0 +1,163 @@
+package com.syncapse.jenkinsci.plugins;
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.Builder;
+import hudson.util.FormValidation;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import net.sf.json.JSONObject;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.kohsuke.stapler.QueryParameter;
+import org.kohsuke.stapler.StaplerRequest;
+
+/**
+ * Sample {@link Builder}.
+ *
+ * <p>
+ * When the user configures the project and enables this builder,
+ * {@link DescriptorImpl#newInstance(StaplerRequest)} is invoked
+ * and a new {@link AWSCloudFormation} is created. The created
+ * instance is persisted to the project configuration XML by using
+ * XStream, so this allows you to use instance fields (like {@link #name})
+ * to remember the configuration.
+ *
+ * <p>
+ * When a build is performed, the {@link #perform(AbstractBuild, Launcher, BuildListener)} method
+ * will be invoked. 
+ *
+ * @author Kohsuke Kawaguchi
+ */
+public class AWSCloudFormation extends Builder {
+
+    /**
+     * The description of the cloud formation stack that will be launched.
+     */
+    private final String description;
+    
+    /**
+     * The json file with the Cloud Formation definition.
+     */
+    private final String cloudFormationRecipe;
+    
+	/**
+     * The parameters to be passed into the cloud formation.
+     */
+    private final String parameters;
+    
+    // Fields in config.jelly must match the parameter names in the "DataBoundConstructor"
+    @DataBoundConstructor
+    public AWSCloudFormation(String description, String cloudFormationRecipe, String parameters) {
+        this.description = description;
+        this.cloudFormationRecipe = cloudFormationRecipe;
+        this.parameters = parameters;
+    }
+
+    @Override
+    public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) {
+        // this is where you 'build' the project
+        // since this is a dummy, we just say 'hello world' and call that a build
+
+        // this also shows how you can consult the global configuration of the builder
+        if(getDescriptor().useFrench())
+            listener.getLogger().println("Bonjour, ");
+        else
+            listener.getLogger().println("Hello, ");
+        return true;
+    }
+
+    // overrided for better type safety.
+    // if your plugin doesn't really define any property on Descriptor,
+    // you don't have to do this.
+    @Override
+    public DescriptorImpl getDescriptor() {
+        return (DescriptorImpl)super.getDescriptor();
+    }
+
+    /**
+     * Descriptor for {@link AWSCloudFormation}. Used as a singleton.
+     * The class is marked as public so that it can be accessed from views.
+     *
+     * <p>
+     * See <tt>views/hudson/plugins/hello_world/HelloWorldBuilder/*.jelly</tt>
+     * for the actual HTML fragment for the configuration screen.
+     */
+    @Extension // this marker indicates Hudson that this is an implementation of an extension point.
+    public static final class DescriptorImpl extends BuildStepDescriptor<Builder> {
+        /**
+         * To persist global configuration information,
+         * simply store it in a field and call save().
+         *
+         * <p>
+         * If you don't want fields to be persisted, use <tt>transient</tt>.
+         */
+        private boolean useFrench;
+
+        /**
+         * Performs on-the-fly validation of the form field 'name'.
+         *
+         * @param value
+         *      This parameter receives the value that the user has typed.
+         * @return
+         *      Indicates the outcome of the validation. This is sent to the browser.
+         */
+        public FormValidation doCheckName(@QueryParameter String value) throws IOException, ServletException {
+            if(value.length()==0)
+                return FormValidation.error("Please set a name");
+            if(value.length()<4)
+                return FormValidation.warning("Isn't the name too short?");
+            return FormValidation.ok();
+        }
+
+        public boolean isApplicable(Class<? extends AbstractProject> aClass) {
+            // indicates that this builder can be used with all kinds of project types 
+            return true;
+        }
+
+        /**
+         * This human readable name is used in the configuration screen.
+         */
+        public String getDisplayName() {
+            return "Create cloud formation stack";
+        }
+
+        @Override
+        public boolean configure(StaplerRequest req, JSONObject formData) throws FormException {
+            // To persist global configuration information,
+            // set that to properties and call save().
+            useFrench = formData.getBoolean("useFrench");
+            // ^Can also use req.bindJSON(this, formData);
+            //  (easier when there are many fields; need set* methods for this, like setUseFrench)
+            save();
+            return super.configure(req,formData);
+        }
+
+        /**
+         * This method returns true if the global configuration says we should speak French.
+         */
+        public boolean useFrench() {
+            return useFrench;
+        }
+    }
+
+    public String getDescription() {
+		return description;
+	}
+
+	public String getCloudFormationRecipe() {
+		return cloudFormationRecipe;
+	}
+
+	public String getParameters() {
+		return parameters;
+	}
+
+}
+
