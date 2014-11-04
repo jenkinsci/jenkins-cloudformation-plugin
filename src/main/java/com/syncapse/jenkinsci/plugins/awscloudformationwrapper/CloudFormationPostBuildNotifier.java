13bae97f613b9f02f874ca081fabfcca75b2f658
amit.gilad <amit.gilad@sizmek.com>
2014-11-04 17:42:48 +0200

    added post build stack creation

---
+++ src/main/java/com/syncapse/jenkinsci/plugins/awscloudformationwrapper/CloudFormationPostBuildNotifier.java
@@ -0,0 +1,130 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.syncapse.jenkinsci.plugins.awscloudformationwrapper;
+
+import static com.syncapse.jenkinsci.plugins.awscloudformationwrapper.CloudFormationPostBuildNotifier.DESCRIPTOR;
+import hudson.EnvVars;
+import hudson.Extension;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.AbstractProject;
+import hudson.model.Action;
+import hudson.model.BuildListener;
+import hudson.tasks.BuildStepDescriptor;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
+import hudson.tasks.Publisher;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Logger;
+import org.kohsuke.stapler.DataBoundConstructor;
+
+/**
+ *
+ * @author amit.gilad
+ */
+public class CloudFormationPostBuildNotifier extends Notifier{
+    	private static final Logger LOGGER = Logger.getLogger(CloudFormationPostBuildNotifier.class.getName());
+	private final List<StackBean> stacks;
+
+	@DataBoundConstructor
+	public CloudFormationPostBuildNotifier(List<StackBean> stacks) {
+		this.stacks = stacks;
+	}
+
+	public List<StackBean> getStacks() {
+		return stacks;
+	}
+
+
+	public BuildStepMonitor getRequiredMonitorService() {
+		return BuildStepMonitor.BUILD;
+	}
+
+	@Override
+	public boolean prebuild(AbstractBuild<?, ?> build, BuildListener listener) {
+		LOGGER.info("prebuild");
+		return super.prebuild(build, listener);
+	}
+
+       
+	@Override
+	public Action getProjectAction(AbstractProject<?, ?> project) {
+		LOGGER.info("getProjectAction");
+		return super.getProjectAction(project);
+	}
+
+	@Override
+	public Collection<? extends Action> getProjectActions(AbstractProject<?, ?> project) {
+		LOGGER.info("getProjectActions");
+		return super.getProjectActions(project);
+	}
+  
+	@Override
+	public boolean perform(AbstractBuild<?, ?> build, Launcher launcher, BuildListener listener) throws InterruptedException, IOException {
+		EnvVars envVars = build.getEnvironment(listener);
+                envVars.overrideAll(build.getBuildVariables());
+		boolean result = true;
+                  
+                 
+		for (StackBean stack : stacks) {
+		final CloudFormation cloudFormation = newCloudFormation(stack,build, envVars, listener.getLogger());	
+                    /*CloudFormation cloudFormation = new CloudFormation(
+					listener.getLogger(),
+					stack.getStackName(),
+					"",
+					new HashMap<String, String>(),
+					0,
+					stack.getParsedAwsAccessKey(envVars),
+					stack.getParsedAwsSecretKey(envVars),
+					stack.getAwsRegion(),
+					false,
+					envVars
+			);*/
+			if(cloudFormation.create()) {
+				LOGGER.info("Success");
+			} else {
+				LOGGER.warning("Failed");
+				result = false;
+			}
+		}
+		return result;
+	}
+	protected CloudFormation newCloudFormation(StackBean stackBean,
+			AbstractBuild<?, ?> build, EnvVars env, PrintStream logger)
+			throws IOException {
+
+		return new CloudFormation(logger, stackBean.getStackName(), build
+				.getWorkspace().child(stackBean.getCloudFormationRecipe())
+				.readToString(), stackBean.getParsedParameters(env),
+				stackBean.getTimeout(), stackBean.getParsedAwsAccessKey(env),
+				stackBean.getParsedAwsSecretKey(env),
+				stackBean.getAwsRegion(), stackBean.getAutoDeleteStack(), env,false,false);
+
+	}
+	@Override
+	public BuildStepDescriptor getDescriptor() {
+		return DESCRIPTOR;
+	}
+
+	@Extension
+	public static final CloudFormationPostBuildNotifier.DescriptorImpl DESCRIPTOR = new CloudFormationPostBuildNotifier.DescriptorImpl();
+
+	public static class DescriptorImpl extends BuildStepDescriptor<Publisher> {
+
+		@Override
+		public String getDisplayName() {
+			return "AWS Cloud Formation";
+		}
+
+		@Override
+		public boolean isApplicable(Class<? extends AbstractProject> jobType) {
+			return true;
+		}
+	}
+}
