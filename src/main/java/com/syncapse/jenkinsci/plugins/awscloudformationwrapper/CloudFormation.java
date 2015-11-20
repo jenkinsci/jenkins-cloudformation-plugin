ed66e309dbe2452bae6cb599cc6b7da2d80277c3
Will Soula <william.soula@drillinginfo.com>
2015-11-20 08:06:43 -0600

    Allow UPDATE_COMPLETE_CLEANUP_IN_PROGRESS status to mean the stack is still running

--- src/main/java/com/syncapse/jenkinsci/plugins/awscloudformationwrapper/CloudFormation.java
+++ src/main/java/com/syncapse/jenkinsci/plugins/awscloudformationwrapper/CloudFormation.java
@@ -376,7 +376,7 @@ public class CloudFormation {
     }
 
     private boolean isStackCreationSuccessful(StackStatus status) {
-        return status == StackStatus.CREATE_COMPLETE || status == StackStatus.UPDATE_COMPLETE;
+        return status == StackStatus.CREATE_COMPLETE || status == StackStatus.UPDATE_COMPLETE || status == StackStatus.UPDATE_COMPLETE_CLEANUP_IN_PROGRESS;
     }
 
     private long getWaitBetweenAttempts (int retries) {
